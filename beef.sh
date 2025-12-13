92929b41a678c71a57bbdf6c63c47375903ceb02
Mr. CSnakke <rahuul.naair@gmail.com>
2025-12-13 16:48:42 +0000

    Update beef.sh

--- beef.sh
+++ beef.sh
@@ -7,4 +7,10 @@ gem install domain_name racc rubocop http-cookie xmlrpc rest-client rushover
 cd $HOME/.local/tools/beef
 ./install
 
+printf "[+] Update the config.yaml file and change credentials to admin/admin and port to 4000 from 3000\n"
 printf "[+] Run ./beef from the folder\n"
+
+echo
+echo
+
+printf "[+] Web URL: http://localhost:4000/ui/panel"
