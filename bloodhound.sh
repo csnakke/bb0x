b193364c9bc6b93ad78b7c0f59d72ef0bf738dad
Mr. CSnakke <rahuul.naair@gmail.com>
2025-12-13 18:27:38 +0000

    Update bloodhound.sh

--- bloodhound.sh
+++ bloodhound.sh
@@ -2,4 +2,8 @@
 
 printf "[+] Installing Bloodhound CE\n"
 sudo apt-get install -y bloodhound bloodhound-ce-python neo4j
+bloodhound-setup
+
+# Update the file sudo vim /etc/bhapi/bhapi.json and update both the credentials - neo4j and admin
+
 bloodhound
