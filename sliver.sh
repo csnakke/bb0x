64fd566a1a90d675d90f34f79e46c8eccbb90bc4
Mr. CSnakke <rahuul.naair@gmail.com>
2025-12-13 16:37:43 +0000

    Create sliver.sh

---
+++ sliver.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+mkdir -p "$HOME/.local/tools/sliver"
+
+wget --quiet --output-file="$HOME/.local/tools/sliver/sliver-client" https://github.com/BishopFox/sliver/releases/latest/download/sliver-client_linux
+wget --quiet --output-file="$HOME/.local/tools/sliver/sliver-server" https://github.com/BishopFox/sliver/releases/latest/download/sliver-server_linux
+
+chmod +x "$HOME/.local/tools/sliver/sliver-client"
+chmod +x "$HOME/.local/tools/sliver/sliver-server"
+
+ln -sf "$HOME/.local/tools/sliver/sliver-client" "$HOME/.local/bin/sliver-client"
+ln -sf "$HOME/.local/tools/sliver/sliver-server" "$HOME/.local/bin/sliver-server"
