447f67a22bf8d92a4d5ffd775555258f4b05040e
Mr. CSnakke <rahuul.naair@gmail.com>
2025-12-13 16:53:56 +0000

    Update sliver.sh

--- sliver.sh
+++ sliver.sh
@@ -4,8 +4,13 @@
 printf "[+] Installing Sliver C2\n"
 mkdir -p "$HOME/.local/tools/sliver"
 
-wget --quiet --output-file="$HOME/.local/tools/sliver/sliver-client" https://github.com/BishopFox/sliver/releases/latest/download/sliver-client_linux
-wget --quiet --output-file="$HOME/.local/tools/sliver/sliver-server" https://github.com/BishopFox/sliver/releases/latest/download/sliver-server_linux
+cd  "$HOME/.local/tools/sliver"
+
+wget --quiet https://github.com/BishopFox/sliver/releases/latest/download/sliver-client_linux
+wget --quiet https://github.com/BishopFox/sliver/releases/latest/download/sliver-server_linux
+
+mv -f sliver-client_linux sliver-client
+mv -f sliver-server_linux sliver-server
 
 chmod +x "$HOME/.local/tools/sliver/sliver-client"
 chmod +x "$HOME/.local/tools/sliver/sliver-server"
