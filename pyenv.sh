c0df89cebec0f3465a27e77c44ca896a42f5443d
Mr. CSnakke <rahuul.naair@gmail.com>
2025-12-22 12:56:50 +0000

    Create pyenv.sh

---
+++ pyenv.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+printf "[+] Installing Python - PyEnv\n"
+sudo apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+curl -fsSL https://pyenv.run | bash
+
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - bash)"
+
+pyenv install 3.10.19
+pyenv local 3.10.19
