11c2c323dbe77dfc82ef375d0899c6a95236cb46
Mr. CSnakke <rahuul.naair@gmail.com>
2025-12-23 14:07:40 +0000

    Update netexec.sh

--- tools/netexec.sh
+++ tools/netexec.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-printf "[+] Installing NetExec/n"
+printf "\n[+] Installing NetExec\n"
 sudo apt-get install -y pipx git
 pipx ensurepath
 pipx install git+https://github.com/Pennyw0rth/NetExec
