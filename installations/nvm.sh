34b33b14bf6937757edbc914867fc0cc8289f53a
Mr. CSnakke <rahuul.naair@gmail.com>
2026-02-03 21:23:36 +0000

    Update nvm.sh

--- installations/nvm.sh
+++ installations/nvm.sh
@@ -4,9 +4,9 @@ printf "[+] Installing NVM\n"
 nvm_version=$(curl -sq https://api.github.com/repos/nvm-sh/nvm/releases/latest | jq -r '.tag_name')
 curl -s -o- "https://raw.githubusercontent.com/nvm-sh/nvm/$nvm_version/install.sh" | bash
 
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="$HOME/.config/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completio
 nvm install --no-progress --lts
 
 printf "[+] Make sure to remove exported values from .zshrc or .bashrc because profile already has it..."
